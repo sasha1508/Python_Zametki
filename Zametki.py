906c6280ee7dd4cc78e0e35d989d846f41242fa8
Александр Ильин <sasha8@list.ru>
2023-09-11 12:12:44 +0300

    Что-то получается

--- Zametki.py
+++ Zametki.py
@@ -1,29 +1,37 @@
-
+import subprocess
+import os
 import json
 import datetime
 
-# объявляем переменные
-title = "Заголовок"
-massage = "Some test string"
+def save(ID, title, massage):
+    # объявляем переменные
+    # title = "Заголовок"
+    # massage = "Some test string"
 
-current_datetime=datetime.datetime.now()
-date = current_datetime.strftime("%c")
+    current_datetime=datetime.datetime.now()
+    date = current_datetime.strftime("%c")
 
+    zametka = {"ID": ID, "title": title, "massage": massage, "date": date}
 
-zametka = {"ID": 1, "title": title, "massage": massage, "date": date}
+    zametki = []
+    zametki.append(zametka)
+    zametki.append(zametka)
 
-zametki = []
-zametki.append(zametka)
-zametki.append(zametka)
+    # создаем словарь
+    mydict = {"zametki": zametki}
 
-# создаем словарь
-mydict = {"zametki": zametki}
+    # сериализуем его в JSON-структуру, как строку
+    x = json.dumps(mydict, ensure_ascii=False, indent=4)
 
-# сериализуем его в JSON-структуру, как строку
-x = json.dumps(mydict, indent=4)
+    print(x)
 
-print(x)
-
-with open('app.json', 'w', encoding='utf-8') as f: f.write(x)
+    with open('zametki.json', 'w', encoding='utf-8') as f: f.write(x)
     
+def read():
+    with open("zametki.json", "r") as read_file:
+        data = json.load(read_file)
+        print(data)
 
+subprocess.call('clear' if os.name =='posix' else 'cls', shell=True)    # Очистка консоли
+save(1, "Заголовок", "Сообщение")
+read()
